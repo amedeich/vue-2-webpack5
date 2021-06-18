e8b4d099bd28a6634444c453e9ff5ae7d038c95b
Alvaro Medina <amedeich@hotmail.com>
2021-06-18 15:15:07 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<style>
+.container {
+  width: 800px;
+  margin: auto;
+  padding: 1rem;
+}
+.detail {
+  display: grid;
+  grid-template-columns: 30% 70%;
+  grid-column-gap: 1rem;
+}
+.detail img {
+  width: 100%;
+}
+</style>
+
+<template>
+<v-app>
+      <v-main>
+        <v-container>Hello world</v-container>
+        <v-btn
+      depressed
+      color="primary"
+    >
+      Primary
+    </v-btn>
+      </v-main>
+    </v-app>
+</template>
+
+<script>
+
+export default {
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { VueLoaderPlugin } = require("vue-loader");
+
+module.exports = {
+  output: {
+    publicPath: "http://localhost:8081/",
+  },
+
+  resolve: {
+    extensions: [".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8081,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require("sass"),
+              indentedSyntax: true, // optional
+            },
+            // Requires >= sass-loader@^8.0.0
+            options: {
+              implementation: require("sass"),
+              sassOptions: {
+                indentedSyntax: true, // optional
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new ModuleFederationPlugin({
+      name: "dogDetail",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./VueApp": "./src/App.vue",
+      },
+      shared: require("./package.json").dependencies,
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
